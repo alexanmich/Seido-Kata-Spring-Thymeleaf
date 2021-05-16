8859a5224913ee87c84c1bfd3ffaee4632a41cdf
michelle <michellealexander161@gmail.com>
2021-05-16 17:33:20 +1200

    init commit

---
+++ src/test/java/seido/kata/KataApplicationTests.java
@@ -0,0 +1,13 @@
+package seido.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
