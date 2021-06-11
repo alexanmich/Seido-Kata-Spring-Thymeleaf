382332b14188b6b677291ad2c01885d0fc8db5ef
michelle <michellealexander161@gmail.com>
2021-06-11 20:41:16 +1200

    annotation change to fix thymeleaf rendering

--- src/main/java/seido/kata/KataController.java
+++ src/main/java/seido/kata/KataController.java
@@ -1,11 +1,11 @@
 package seido.kata;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class KataController {
 	
 	private KataRepository kataRepository = new KataRepository();
